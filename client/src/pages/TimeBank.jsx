dd99afc60e939d79641130e616298e78090b1f9a
SaherKawas <saher.kawas@gmail.com>
2025-07-27 16:26:02 +0300

    Deepseek and timebank

--- client/src/pages/TimeBank.jsx
+++ client/src/pages/TimeBank.jsx
@@ -187,17 +187,8 @@ const TimeBank = () => {
         fetchServices();
     }, [activeTab]);
 
-
-
-
-
-
-
-
-
-
     return (
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen bg-gray-50 pt-6">
             {/* Header */}
             <div className="bg-white shadow-sm border-b">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
